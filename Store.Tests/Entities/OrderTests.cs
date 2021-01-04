86625165c69d8cd3f92dbf705525dd1b5299b1f1
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-04 16:38:43 -0300

    3.4 Testando o Status do Pedido

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -8,12 +8,15 @@ namespace Store.Tests.Domain
     [TestClass]
     public class OrderTests
     {
+        private readonly Customer _customer = new Customer("Wellington Massola", "wellingtonmassola@gmail.com");
+        private readonly Product _product = new Product("Produto 1", 10, true);
+        private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
         {
-            var customer = new Customer("Wellington Massola", "wellingtonmassola@gmail.com");
-            var order = new Order(customer, 0, null);
+            var order = new Order(_customer, 0, null);
             Assert.AreEqual(8, order.Number.Length);
         }
 
@@ -21,7 +24,8 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
         {
-            Assert.Fail();
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
         }
 
         [TestMethod]
